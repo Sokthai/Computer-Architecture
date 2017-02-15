e49e92f1947cf70421d0afb36c0c59b360b4c608
sokthai <sokthaitang@gmail.com>
2017-02-15 01:40:41 -0500

    fix hw3

--- hw3/hw3.asm
+++ hw3/hw3.asm
@@ -1,44 +1,44 @@
-;  NOTE: AC & AX may the same thing here
 
-loop:	lodd myCounter:		; Load the counter of numbers to Accumulator (AX)
-		jzer done:		; If its zero no more nums to do, goto done (check the ax if it is 0 then jump to lebel done)
-		subd constant1:		; Take one away from count (the accumuator of value counter - cons1 .  => counter - cons1)
-		stod myCounter:	; Pass remianing numbers to do (store the result from substraction to counter . => count = count - cons1)
-one:	lodd daddr: 	; Load pointer to array of numbers to do (load poiter array to accumuator (AX))
-		pshi			; Push arg pointer onto stack
-		addd constant1:		; Increment
-		stod daddr:		; Store pointer for next fib number to do
-		call fib:		; Call fib with arg on stack
-		insp 1			; Clear stack on fib
-two:	push			; Put return on stack (put the result frmo f(n) from AC/AX to stack 
-		lodd raddr:		; load pointer to result array
-		popi			; Pop result off stack into result array
-		addd constant1:		; Increment
-		stod raddr:		; store pointer for next number to do
-		jump loop:		; Start loop again
-fib:	lodl 1			; Load arg from stack
-		jzer fibz: 		; if 0 go to fibz
-		subd constant1:		; decrement argument
-		jzer fibo:		; if 0 go to fibo
-		push			; else push arg - 1 for recursive call
-		call fib:		; Recursive call
-		push			; push result onto stack
-		lodl 1 			; load arg - 1 from stack
-		subd constant1: 	; accumulator now arg - 2
-		push 			; push arg - 2 onto stack
-		call fib: 		; call fib function recursively
-clear:	insp 1			; Clear arg - 2 from stack
-		addl 0 			; put result in accumulator
-		insp 2 			; Clear arguements from stack
-		retn			; Return
-fibz: 	lodd constant0: 	; set accumulator = 0
-		retn 			; Return
-fibo:	lodd constant1:		; set accumulaotr = 1
-		retn			; Return
-done:	halt			; Halt program execution
 
-	;; Data Structures
-.LOC 100			; Locate data begining at 100
+loop:	lodd myCounter:			; Load  counter of numbers to Accumulator (ac)
+		jzer done:				; If zero, then done (check the ac if it is 0 then jump to lebel done)
+		subd constant1:			; decrement 1 from myCounter (the accumuator of value counter - cons1 .  => counter - cons1)
+		stod myCounter:			; store the result from substraction to counter . => count = count - cons1)
+one:	lodd daddr: 			; load poiter array to accumuator (ac))
+		pshi					; push argument to stack
+		addd constant1:			; Increment 1
+		stod daddr:				; Store pointer array for next fib 
+		call fib:				; Call fib with the arg that is on stack
+		insp 1					; Clear stack 
+two:	push					; Put return on stack (put the result from f(n) from ac to stack )
+		lodd raddr:				; load result pointer array
+		popi					; Pop the stack that store the result into array
+		addd constant1:			; Increment 1
+		stod raddr:				; store pointer array for later use
+		jump loop:				; resurcive the function
+fib:	lodl 1					; Load the arg from stack
+		jzer fibZero: 			; if counter is zero, go to fibZero
+		subd constant1:			; decrement argument (f(n - 1))
+		jzer fibOne:			; if 0 go to fibOne
+		push					;  push arg - 1 for recursive call
+		call fib:				; Recursive call to fib
+		push					; push the result onto stack
+		lodl 1 					; load arg - 1 from stack
+		subd constant1: 		; ac is arg - 2 (f(n - 2))
+		push 					; push arg - 2 onto stack
+		call fib: 				; resurcively call fib again
+clear:	insp 1					; Clear the arg - 2 (f(n -2)) from stack
+		addl 0 					; store new result in accumulator/ac
+		insp 2 					; Clear all arguements from stack
+		retn					; Return function
+fibZero: 	lodd constant0: 	; set ac = 0
+		retn 					; Return
+fibOne:	lodd constant1:			; set ac = 1
+		retn					; Return
+done:	halt					; Halt program execution
+
+								
+.LOC 100						; data begin at 100
 data:	
 	3
 	9
